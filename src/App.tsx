3eed6057e0317bc4674bca3d35ce373aaf02b0d9
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2024-10-31 12:49:55 +0530

    Right click disabled code added but commented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import SelectedCompany from "./Components/SelectCompany/SelectCompany";
+//import React, { useEffect } from 'react';
+import SelectedCompany from './Components/SelectCompany/SelectCompany';
 
-function App() {
+const App: React.FC = () => {
+  // useEffect(() => {
+  //   const handleContextMenu = (event: MouseEvent) => {
+  //     event.preventDefault();
+  //   };
+
+  //   document.addEventListener('contextmenu', handleContextMenu);
+
+  //   return () => {
+  //     document.removeEventListener('contextmenu', handleContextMenu);
+  //   };
+  // }, []);
+
   return (
     <div>
       <SelectedCompany />
     </div>
-  );  
+  );
 }
 
 export default App;
